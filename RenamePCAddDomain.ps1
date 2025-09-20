e57400f710c7d9e3eb5988dc8370eeca09b54f79
Adam <101077369+adampapaya@users.noreply.github.com>
2025-09-19 20:56:03 -0700

    Delete RenamePCAddDomain.ps1

--- RenamePCAddDomain.ps1
+++
@@ -1,6 +0,0 @@
-$NewComputerName = read-host 'Computer Name'
-$DomainName = "niagarawater.com"
-$Credentials = get-credential
-
-Rename-Computer $NewComputerName -force
-Add-Computer -Domain $DomainName -NewName $NewComputerName -Credential $Credentials -Restart -Force
